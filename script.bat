d32a9d3984db7de1f5bd9d42e81a5c9376740215
wicivo <116191734+wicivo@users.noreply.github.com>
2024-04-26 11:52:34 +0500

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+if %errorlevel% neq 0 (
+    cd /d "%ProgramFiles%\Microsoft Office\Office16"
+)
+for /f %%x in ('dir /b "..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms"') do (
+    cscript ospp.vbs /inslic:"..\root\Licenses16\%%x"
+)
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /unpkey:6F7TH >nul
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:107.175.77.7
+cscript ospp.vbs /act
